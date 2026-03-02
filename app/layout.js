49a8257d1041c0a7492f8e8849b6def2511cb1ab
sank0o <sankoo1997s@gmail.com>
2026-03-02 10:11:02 +0300

    Update metadata title and description

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: ' | دودو الحلوا ',
-  description: 'شنو من حلقوم انتِ',
+  title: ' | Elite ',
+  description: 'store',
 }
 
 export default function RootLayout({ children }) {
